5d97ff2b11fd0b089879efa1394a3129d0c0e1f6
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2022-06-07 16:57:13 +0400

    hometasks added

---
+++ JuanProject/JuanProject/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,114 @@
+﻿using JuanProject.DAL;
+using JuanProject.Models;
+using JuanProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JuanProject.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class SliderController : Controller
+    {
+        
+
+        private AppDbContext _context;
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+
+            HomeViewModel homeViewModel = new HomeViewModel
+            {
+                Sliders = _context.Sliders.ToList(),
+                Services = _context.Services.ToList()
+            };
+
+
+            return View(homeViewModel);
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Slider slider)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.Sliders.Add(slider);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            Slider slider = _context.Sliders.FirstOrDefault(x => x.Id == id);
+
+            if (slider == null)
+                return RedirectToAction("error", "dashboard");
+
+            return View(slider);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Slider slider)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            Slider existSlider = _context.Sliders.FirstOrDefault(x => x.Id == slider.Id);
+
+            if (existSlider == null)
+                return RedirectToAction("error", "dashboard");
+
+            existSlider.SubTitle = slider.SubTitle;
+            existSlider.Title = slider.Title;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            Slider author = _context.Sliders.FirstOrDefault(x => x.Id == id);
+
+            if (author == null)
+                return RedirectToAction("error", "dashboard");
+
+            return View(author);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Slider slider)
+        {
+            Slider existSlider = _context.Sliders.FirstOrDefault(x => x.Id == slider.Id);
+
+
+            if (existSlider == null)
+                return RedirectToAction("error", "dashboard");
+
+            _context.Sliders.Remove(existSlider);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+    }
+}
